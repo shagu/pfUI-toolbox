0ec96dfeefac77c1a79000cacb256dcbeb6a750d
shagu <ericmauser@googlemail.com>
2017-11-18 00:38:32 +0100

    dbc-translate: add browse mode and esES locales

--- tools/dbc-translate.sh
+++ tools/dbc-translate.sh
@@ -7,36 +7,76 @@ function SearchTranslation () {
   echo -e " $1 ($id)"
   echo "============================================="
 
-  for locale in enUS deDE frFR koKR ruRU zhCN; do
+  for locale in enUS koKR frFR deDE zhCN esES ruRU; do
     case "$locale" in
       "enUS")
         echo -e "$locale:\t$(cut -d , -f 1,121-130 Spell_${locale}.dbc.csv | grep "^$id," | cut -d , -f 2)"
       ;;
-      "deDE")
-        echo -e "$locale:\t$(cut -d , -f 1,121-130 Spell_${locale}.dbc.csv | grep "^$id," | cut -d , -f 5)"
+      "koKR")
+        echo -e "$locale:\t$(cut -d , -f 1,121-130 Spell_${locale}.dbc.csv | grep "^$id," | cut -d , -f 3)"
       ;;
       "frFR")
         echo -e "$locale:\t$(cut -d , -f 1,121-130 Spell_${locale}.dbc.csv | grep "^$id," | cut -d , -f 4)"
       ;;
-      "koKR")
-        echo -e "$locale:\t$(cut -d , -f 1,121-130 Spell_${locale}.dbc.csv | grep "^$id," | cut -d , -f 3)"
-      ;;
-      "ruRU")
-        echo -e "$locale:\t$(cut -d , -f 1,121-130 Spell_${locale}.dbc.csv | grep "^$id," | cut -d , -f 2)"
+      "deDE")
+        echo -e "$locale:\t$(cut -d , -f 1,121-130 Spell_${locale}.dbc.csv | grep "^$id," | cut -d , -f 5)"
       ;;
       "zhCN")
         echo -e "$locale:\t$(cut -d , -f 1,121-130 Spell_${locale}.dbc.csv | grep "^$id," | cut -d , -f 6)"
       ;;
+      "esES")
+        echo -e "$locale:\t$(cut -d , -f 1,121-130 Spell_${locale}.dbc.csv | grep "^$id," | cut -d , -f 8)"
+      ;;
+      "ruRU")
+        echo -e "$locale:\t$(cut -d , -f 1,121-130 Spell_${locale}.dbc.csv | grep "^$id," | cut -d , -f 2)"
+      ;;
     esac
   done
 }
+
+function BrowseTranslation () {
+  grep "$spell" Spell_enUS.dbc.csv | cut -d , -f 1 | while read id; do
+    echo "============================================="
+    echo -e " $(cut -d , -f 1,121-130 Spell_enUS.dbc.csv | grep "^$id," | cut -d , -f 2) ($id)"
+    echo "============================================="
+
+    for locale in enUS koKR frFR deDE zhCN esES ruRU; do
+      case "$locale" in
+        "enUS")
+          echo -e "$locale:\t$(cut -d , -f 1,121-130 Spell_${locale}.dbc.csv | grep "^$id," | cut -d , -f 2)"
+        ;;
+        "koKR")
+          echo -e "$locale:\t$(cut -d , -f 1,121-130 Spell_${locale}.dbc.csv | grep "^$id," | cut -d , -f 3)"
+        ;;
+        "frFR")
+          echo -e "$locale:\t$(cut -d , -f 1,121-130 Spell_${locale}.dbc.csv | grep "^$id," | cut -d , -f 4)"
+        ;;
+        "deDE")
+          echo -e "$locale:\t$(cut -d , -f 1,121-130 Spell_${locale}.dbc.csv | grep "^$id," | cut -d , -f 5)"
+        ;;
+        "zhCN")
+          echo -e "$locale:\t$(cut -d , -f 1,121-130 Spell_${locale}.dbc.csv | grep "^$id," | cut -d , -f 6)"
+        ;;
+        "esES")
+          echo -e "$locale:\t$(cut -d , -f 1,121-130 Spell_${locale}.dbc.csv | grep "^$id," | cut -d , -f 8)"
+        ;;
+        "ruRU")
+          echo -e "$locale:\t$(cut -d , -f 1,121-130 Spell_${locale}.dbc.csv | grep "^$id," | cut -d , -f 2)"
+        ;;
+      esac
+    done
+  done
+}
+
+
+
 
 if [ "$1" = "-r" ]; then
   while true; do
     echo -n "Search: "
     read spell
     echo
-    SearchTranslation "$spell"
+    BrowseTranslation "$spell"
     echo
     echo
   done
