0badb21a69c8cc5024aabf8edb76505b8449c71a
shagu <ericmauser@googlemail.com>
2019-03-18 19:11:04 +0100

    translations: fix translation table updater

--- tools/update-translations.sh
+++ tools/update-translations.sh
@@ -5,13 +5,13 @@ for locale in "deDE" "enUS" "frFR" "koKR" "ruRU" "zhCN" "zhTW" "esES"; do
 
   echo "pfUI_translation[\"$locale\"] = {" | tee $file_new
 
-  cat modules/*.lua | sed 's/T\[/\nT\[/g' | grep "T\[" | sed 's/.*T\["\(.*\)"\].*/\1/' | while read -r entry; do
+  cat modules/* | sed "s/\(T\[\"\)/\n\1/" | grep -oP "T\[\".*?\"]" | sed 's/T\["\(.*\)"\]/\1/' | sort | uniq | while read -r entry; do
     old=$(grep -F "\"$entry\"" $file 2> /dev/null)
     if [ -z "$old" ] ; then
       old="  [\"$entry\"] = nil,"
     fi
     echo "$old"
-  done | sort | uniq | tee -a $file_new
+  done | tee -a $file_new
   echo "}" | tee -a $file_new
 
   mv $file_new $file
